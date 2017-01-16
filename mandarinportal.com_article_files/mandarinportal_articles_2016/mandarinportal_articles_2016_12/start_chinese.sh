675275471554a4d2ccfaa5ca54c80ac0945bcf40
mandarinportal <mandarinportaltwitter@yandex.com>
2017-01-16 17:54:17 -0600

    Create start_chinese.sh

---
+++ mandarinportal.com_article_files/mandarinportal_articles_2016/mandarinportal_articles_2016_12/start_chinese.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This file was created by mandarinportal.com
+#It is released under the GPLv3 license
+#You are free to copy it, modify it, and/or share it
+#Please give credit to mandarinportal.com
+
+#this script reads in Chinese input and assigns it to a variable
+#called chinese_words
+#then grep searches the content and outputs the definition
+#This script only words with Traditional Chinese Characters / Words
+echo "Enter the Traditional Chinese Character or Word that you want to search:"
+read chinese_words
+grep "^$chinese_words " current_cc-cedict.txt
