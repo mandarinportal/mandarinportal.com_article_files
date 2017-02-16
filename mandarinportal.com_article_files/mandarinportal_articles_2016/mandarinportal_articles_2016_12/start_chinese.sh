4dfe4fc17a32f031d730b6e150a29d61a76e1d4e
mandarinportal.com <mandarinportaltwitter@yandex.com>
2017-02-15 21:57:02 -0600

    Update start_chinese.sh

--- mandarinportal.com_article_files/mandarinportal_articles_2016/mandarinportal_articles_2016_12/start_chinese.sh
+++ mandarinportal.com_article_files/mandarinportal_articles_2016/mandarinportal_articles_2016_12/start_chinese.sh
@@ -3,6 +3,7 @@
 #It is released under the GPLv3 license
 #You are free to copy it, modify it, and/or share it
 #Please give credit to mandarinportal.com
+#This file comes from the article based at https://mandarinportal.com/a-script-to-easily-search-traditional-chinese-words-with-cc-cedict-in-linux/
 
 #this script reads in Chinese input and assigns it to a variable
 #called chinese_words
