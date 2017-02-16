7cfbac3de74c4597329fd2759d940cc7a778f763
mandarinportal.com <mandarinportaltwitter@yandex.com>
2017-02-15 20:56:55 -0600

    Create search_simplified_chinese.sh

---
+++ mandarinportal.com_article_files/mandarinportal_articles_2017/mandarinportal_articles_2017_02/search_simplified_chinese.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This file was created by mandarinportal.com
+#It is released under the GPLv3 license
+#You are free to copy it, modify it, and/or share it
+#Please give credit to mandarinportal.com
+#This file is for the article located at mandarinportal.com
+
+#this script reads in Simplifed Chinese input and assigns it to a variable called chinese_words
+#then grep searches the content and outputs the definition
+#This script only words with Simplified Chinese Characters / Words
+echo "Enter the Simplified Chinese Character or Word that you want to search:"
+read chinese_words
+length=${#chinese_words}
+spaces=$(for ((w=1;w<=$length;w++)); do echo -n "."; done)
+grep "^$(echo $spaces) $chinese_words " current_cc-cedict.txt
